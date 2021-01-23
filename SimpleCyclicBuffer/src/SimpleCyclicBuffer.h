bfba5315a995927dc08c9a4125958da4bf6a1099
Lior <lior.timor@flex.com>
2021-01-23 19:09:27 +0200

    Added tests. Pending: Moving tests to gTest or CppUTest

--- SimpleCyclicBuffer/src/SimpleCyclicBuffer.h
+++ SimpleCyclicBuffer/src/SimpleCyclicBuffer.h
@@ -13,6 +13,7 @@
 #define SIMPLECYCLICBUFFER_H_
 
 #include <array>
+#include <exception>
 
 template <class T, int bufferSize>
 class SimpleCyclicBuffer
@@ -21,23 +22,70 @@ class SimpleCyclicBuffer
 		std::array<T, bufferSize> _cycBuffer;
 		int _head = 0;
 		int _tail = 0;
+		int _count = 0;
+
+		/// Advances a head/tail pointer
+		static inline void advancePointer(int &p)
+		{
+			p = (p + 1) % bufferSize;
+		}
 
 	public:
 		SimpleCyclicBuffer() = default;
 
+		/// Clears the buffer
+		void clear()
+		{
+			_head = _tail = _count = 0;
+		}
+
+
 		/// Pushes an item into the cyclic buffer using the move semantics.
-		bool push(T&& item);
+		bool push(T&& item)
+		{
+			if (bufferSize < (_count + 1))
+				throw std::overflow_error("Buffer overflow") ;
+
+			_cycBuffer[_head] = std::move(item);
+			advancePointer(_head);
+			_count++;
+			return true;
+		}
+
 		/// Pushes an item into the cyclic buffer (invoking copy).
-		bool push(T& item);
+		bool push(const T& item)
+		{
+			T temp = item;
+
+			return push(std::move(temp));
+		}
+
+
 		/// Pops an item from the buffer using the move semantics
-		T&& pop();
+		T& pop()
+		{
+			if (-1 == (_count - 1))
+				throw std::underflow_error("Buffer is empty") ;
+
+			const auto old_tail = _tail;
+			advancePointer(_tail);
+			_count--;
+
+			return _cycBuffer[old_tail];
+		}
+
+
 		/// Peeks into the next item on the list
-		const bool peek(T&& item);
-		/// Peeks into the next item on the list (invokes a copy)
-		const bool peek(T& item);
+		const T& peek()
+		{
+			if (0 == _count)
+				throw std::underflow_error("Buffer is empty") ;
+
+			return _cycBuffer[_tail];
+		}
 
 		/// Returns the number of items in the list
-		int len() { return ((bufferSize) + (_head - _tail)) % bufferSize; }
+		int len() { return _count; }
 
 		/// Returns the total size of the buffer (in number of items)
 		constexpr int size() { return bufferSize; }
--- SimpleCyclicBuffer/src/TestLib.cpp
+++ SimpleCyclicBuffer/src/TestLib.cpp
@@ -3,13 +3,13 @@
 // Author      : Lior Timor
 // Version     :
 // Copyright   : MIT
-// Description : Test SimpleCyclicBuffer - TODO: Need to swap with CppUTest
+// Description : Test SimpleCyclicBuffer - TODO: Need to swap with CppUTest/Gtest
 //============================================================================
 
 #include <iostream>
 using namespace std;
 
-
+#include <list>
 #include "SimpleCyclicBuffer.h"
 
 //TODO: Swap with a CppUTest
@@ -18,18 +18,152 @@ using namespace std;
 
 bool testSimpleBuffer()
 {
-	SimpleCyclicBuffer<int, 100> intBuffer;
+	constexpr int BUFFER_SIZE = 100;
+	SimpleCyclicBuffer<int, BUFFER_SIZE> intCyclicBuffer;
+
+	int a = 20;
+
+	_assert(intCyclicBuffer.size() == 100);
+	_assert(intCyclicBuffer.len() == 0);
+
+	//---- Test partial insert (add by reference) ----
+	for (int i=0; i < 20; i++)
+	{
+		a = i;
+		intCyclicBuffer.push(a);
+	}
+	_assert(intCyclicBuffer.len() == 20);
+	for (int i=0; i < 20; i++)
+	{
+		a = intCyclicBuffer.pop();
+		_assert(a == i);
+	}
+	_assert(intCyclicBuffer.len() == 0);
+
+	//---- Test partial insert (add by move) ----
+	for (int i=0; i < 20; i++)
+	{
+		a = i;
+		intCyclicBuffer.push(std::move(a));
+	}
+	_assert(intCyclicBuffer.len() == 20);
+	for (int i=0; i < 20; i++)
+	{
+		a = intCyclicBuffer.pop();
+		_assert(a == i);
+	}
+	_assert(intCyclicBuffer.len() == 0);
 
 
-	_assert(intBuffer.size() == 100);
-	_assert(intBuffer.len() == 0);
+	//---- Test FULL insert (add by move) ----
+	for (int i=0; i < BUFFER_SIZE; i++)
+	{
+		a = i;
+		intCyclicBuffer.push(std::move(a));
+	}
+	_assert(intCyclicBuffer.len() == BUFFER_SIZE);
+	for (int i=0; i < BUFFER_SIZE; i++)
+	{
+		a = intCyclicBuffer.pop();
+		_assert(a == i);
+	}
+	_assert(intCyclicBuffer.len() == 0);
+
+
+	//---- Test PEEK ----
+	for (int i=0; i < BUFFER_SIZE; i++)
+	{
+		a = i;
+		intCyclicBuffer.push(a);
+	}
+	_assert(intCyclicBuffer.len() == BUFFER_SIZE);
+	for (int i=0; i < BUFFER_SIZE; i++)
+	{
+		a = intCyclicBuffer.peek();
+		_assert(a == i);
+		a = intCyclicBuffer.pop();
+	}
+	_assert(intCyclicBuffer.len() == 0);
+
+
+	//---- Test Clear ----
+	for (int i=0; i < BUFFER_SIZE/2; i++)
+	{
+		a = i;
+		intCyclicBuffer.push(a);
+	}
+	intCyclicBuffer.clear();
+	_assert(intCyclicBuffer.len() == 0);
+
+
+	//---- Test Mixed push/pop ----
+	std::list<int> backlog;
+	int count = 0;
+	for (int iteration = 0; iteration < 5; iteration++)
+	{
+		for (int i=0; i < (BUFFER_SIZE / 3); i++)
+		{
+			a = i * (iteration+i);
+			intCyclicBuffer.push(a);
+			backlog.push_front(a);
+
+			count++;
+		}
+		for (int i=0; i < (BUFFER_SIZE / 3); i++)
+		{
+			a = intCyclicBuffer.peek();
+			int b = intCyclicBuffer.pop();
+			int e = backlog.back();
+
+			_assert(a == b);
+			_assert(a == e);
+			backlog.pop_back();		//Extract from the list
+			count--;
+		}
+	}
+	_assert(count == intCyclicBuffer.len());
+
+	//Test Underflow
+	bool underflowCaught = false;
+	try
+	{
+		for (int i=0; i <5; i++)
+		{
+			intCyclicBuffer.push(1);
+		}
+		for (int i=0; i < 6; i++)
+		{
+			intCyclicBuffer.pop();
+		}
+	} catch (const std::underflow_error&)
+	{
+		underflowCaught = true;
+	}
+	_assert(true == underflowCaught);
+
+
+
+	//Test Overflow
+	bool overflowCaught = false;
+	try
+	{
+		for (int i=0; i <intCyclicBuffer.size() + 1; i++)
+		{
+			intCyclicBuffer.push(1);
+		}
+	} catch (const std::overflow_error&)
+	{
+		overflowCaught = true;
+	}
+	_assert(true == overflowCaught);
+
 
 	return true;
 }
 
 int main() {
 
-	cout << "running tests (reminder - replace with CppUTest)\n\n";
+	cout << "running tests (reminder - replace with CppUTest/GTest)\n\n";
 
 
 	if (testSimpleBuffer() == true)
